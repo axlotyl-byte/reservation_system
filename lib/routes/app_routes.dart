d41b4c0a1f6a90cd7919cb58e642d930d20c754b
axlotyl-byte <altheagavilo82@gmail.com>
2025-12-20 01:01:32 +0300

    app routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:reservation_system/features/product/presentation/screens/main_screen.dart';
+import 'package:reservation_system/features/product/presentation/screens/product_detail_screen.dart';
 import 'package:reservation_system/features/product/presentation/screens/order_confirmation_screen.dart';
 import 'package:reservation_system/features/product/presentation/screens/order_history_screen.dart';
 import 'package:reservation_system/features/product/presentation/screens/order_tracking_screen.dart';
@@ -8,6 +9,7 @@ import 'package:reservation_system/features/product/presentation/screens/checkou
 
 enum AppRoute {
   home('/'),
+  productDetail('/product/:id'),
   orderConfirmation('/order-confirmation'),
   orderHistory('/my-orders'),
   orderTracking('/order-tracking/:orderId'),
@@ -28,6 +30,17 @@ class AppRouter {
           path: AppRoute.home.path,
           name: AppRoute.home.name,
           builder: (context, state) => const MainScreen(),
+          routes: [
+            // Product Detail
+            GoRoute(
+              path: 'product/:id',
+              name: AppRoute.productDetail.name,
+              builder: (context, state) {
+                final productId = state.pathParameters['id']!;
+                return ProductDetailScreen(productId: productId);
+              },
+            ),
+          ],
         ),
 
         // Order Confirmation (uses query parameter orderId)
@@ -57,14 +70,12 @@ class AppRouter {
           builder: (context, state) => const OrderHistoryScreen(),
         ),
 
-        // Checkout (optional query parameter cartId)
         // Checkout
         GoRoute(
           path: AppRoute.checkout.path,
           name: AppRoute.checkout.name,
           builder: (context, state) => const CheckoutScreen(),
         ),
-
       ],
       errorBuilder: (context, state) => _buildErrorScreen(context),
     );
